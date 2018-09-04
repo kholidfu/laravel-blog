2d44db9c33c6124a015d8b5b89ab54b96c50c801
kholidfu <sopier@gmail.com>
2018-09-04 19:35:05 +0700

    delete custom user login

--- resources/views/mylayout/nav.blade.php
+++ resources/views/mylayout/nav.blade.php
@@ -10,10 +10,10 @@
             <a class="nav-link" href="{{ URL::route('index') }}">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ URL::route('myuser_register') }}">Register</a>
+            <a class="nav-link" href="{{ URL::route('register') }}">Register</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ URL::route('myuser_login') }}">Login</a>
+            <a class="nav-link" href="{{ URL::route('login') }}">Login</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ URL::route('home') }}">Dashboard Home</a>
--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,6 @@ Route::get('/', array('as' => 'index', function () {
     return view('mylayout.index');
 }));
 
-Route::group(['prefix' => 'user'], function() {
-	Route::get('/register', array('as' => 'myuser_register', function() {
-		return view('myuser.register');
-	}));
-	
-	Route::get('/login', array('as' => 'myuser_login', function() {
-		return view('myuser.login');
-	}));
-	Route::post('/login', 'myUser\myUserController@login');
-	Route::get('/logout', 'myUser\myUserController@logout');
-});
-
-Route::get('/users', array('as' => 'users', function() {
-	// pull data straight from App\User controller
-	$users = User::all();
-	return $users;
-}));
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
