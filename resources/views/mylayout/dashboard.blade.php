28726cac7264b2d8cea93eef23b6121ac073e056
kholidfu <sopier@gmail.com>
2018-09-06 03:33:35 +0700

    adding dashboard feature

--- resources/views/mylayout/dashboard.blade.php
+++ resources/views/mylayout/dashboard.blade.php
@@ -4,7 +4,12 @@
   <div class="row">
     <div class="col-md-8" style="margin-top: 50px;">
       <h2>Please logout</h2>
-      <a class="btn btn-primary" href="{{ URL::route('logout') }}">Logout</a>
+      <p>This is your dashboard. Here, you will able to:</p>
+      <ul>
+        <li>Create Categories</li>
+        <li>Save Document</li>
+        <li>Share Document</li>
+      </ul>
     </div>
   </div>
   @endsection
